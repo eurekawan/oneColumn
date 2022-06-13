686e9ac35ba53a971d471a22c4860e66b1d91f04
eurekawan <wanyq1998@outlook.com>
2022-06-13 14:48:54 +0800

    useClickOutside done

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,6 +1,6 @@
 <script lang='ts'>
-import { defineComponent,ref,onMounted,onUnmounted } from 'vue'
-
+import { defineComponent, ref, watch} from 'vue'
+import useClickOutside from '../hooks/useClickOutside'
 
 export default defineComponent({
   name: 'Dropdown',
@@ -16,18 +16,24 @@ export default defineComponent({
     const toggleOpen = () => {
       isOpen.value = !isOpen.value
     }
-    const hander = (e: MouseEvent) => {
-      if (dropdownRdef.value) {
-        if (!dropdownRdef.value.contains(e.target as HTMLElement) && isOpen.value) {
-          isOpen.value = false
-        }
+    const isClickOutside = useClickOutside(dropdownRdef)
+    // const handler = (e: MouseEvent) => {
+    //   if (dropdownRdef.value) {
+    //     if (!dropdownRdef.value.contains(e.target as HTMLElement) && isOpen.value) {
+    //       isOpen.value = false
+    //     }
+    //   }
+    // }
+    // onMounted(() => {
+    //   document.addEventListener('click',handler)
+    // })
+    // onUnmounted(() => {
+    //   document.removeEventListener('click',handler)
+    // })
+    watch(isClickOutside,() => {
+      if (isOpen.value && isClickOutside.value) {
+        isOpen.value = false
       }
-    }
-    onMounted(() => {
-      document.addEventListener('click',hander)
-    })
-    onUnmounted(() => {
-      document.removeEventListener('click',hander)
     })
     return {
       isOpen,
